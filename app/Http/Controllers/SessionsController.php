1f430d1ab32d01b67b44f36575295e7ebded14d5
fengzhu2013 <913346548@qq.com>
2018-07-18 07:54:09 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+class SessionsController extends Controller
+{
+    public function create()
+    {
+    	return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+    	$credentials = $this->validate($request,[
+    		'email'	=> 'required|max:50|email',
+    		'password' => 'required'
+    	]);
+    	if (Auth::attempt($credentials,$request->has('remember'))) {
+    		session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+    	} else {
+
+    		session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
+           	return redirect()->back()->withInput($request->except('password'));
+    	}
+    }
+
+
+    public function destroy()
+    {
+    	Auth::logout();
+    	session()->flash('success','logout successful');
+    	return redirect('login');
+    }
+
+}
