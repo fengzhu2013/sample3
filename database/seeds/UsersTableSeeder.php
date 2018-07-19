2542a675365a55b4ccd2a8796a0133c03dcfb5fa
fengzhu2013 <913346548@qq.com>
2018-07-19 07:21:52 +0000

    what a error

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password','remeber_token'])->toArray());
+
+        $user = User::first();
+        $user->name = 'peter';
+        $user->email = 'peter@qq.com';
+        $user->password = bcrypt('123456');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
